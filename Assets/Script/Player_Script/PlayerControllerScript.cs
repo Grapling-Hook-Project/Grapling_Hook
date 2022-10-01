53615422233b8ccbf3894b736edc11b1690c6711
yuto-0122 <90639451+yuto-0122@users.noreply.github.com>
2022-10-02 04:43:51 +0900

    ワイヤーの処理を実装しました。
    プレイヤーの挙動、切り離しを追加を行います。

--- Assets/Script/Player_Script/PlayerControllerScript.cs
+++ Assets/Script/Player_Script/PlayerControllerScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerCharcterController : MonoBehaviour
+public class PlayerControllerScript : MonoBehaviour
 {
     private const float NORMAL_FOV = 60f;
     private const float HOOKSHOT_FOV = 100f;
@@ -18,8 +18,6 @@ public class PlayerCharcterController : MonoBehaviour
     private float charcterVelocityY;
     private float hookshotSize;
     private Camera playerCamera;
-    private CameraScript cameraFov;
-    public WireColisionScript wireColisionScript;
     private State state;
     private Vector3 shotPoint;
     private Vector3 characterVelocityMomentum;
@@ -29,14 +27,15 @@ public class PlayerCharcterController : MonoBehaviour
         Normal,
         HookshotThrown,
         HookshotFlyingPlayer,
-        HookshotStop
     }
     private void Awake()
     {
         //取得
         characterController = GetComponent<CharacterController>();
         playerCamera = transform.Find("Main Camera").GetComponent<Camera>();
+        //マウス固定
         Cursor.lockState = CursorLockMode.Locked;
+        //変更
         state = State.Normal;
         hookshotTransform.gameObject.SetActive(false);
     }
@@ -74,15 +73,14 @@ public class PlayerCharcterController : MonoBehaviour
 
         playerCamera.transform.localEulerAngles = new Vector3(cameraVerticalAngle, 0, 0);
     }
-
     private void CharacterMovement()
     {
-        float fSpeed = 20f;
+        float moveSpeed = 20f;
 
         //Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
         Vector3 right = Camera.main.transform.TransformDirection(Vector3.right);
         Vector3 moveDirection = Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        moveDirection *= fSpeed;
+        moveDirection *= moveSpeed;
 
         //Vector3 characterVelocity = transform.right * moveX * moveSpeed + transform.forward * moveZ * moveSpeed;
 
@@ -90,7 +88,7 @@ public class PlayerCharcterController : MonoBehaviour
         {
             charcterVelocityY = 0f;
 
-            if (TestInputJump())
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 float jumpSeed = 30f;
                 charcterVelocityY = jumpSeed;
@@ -106,47 +104,7 @@ public class PlayerCharcterController : MonoBehaviour
         moveDirection += characterVelocityMomentum;
 
         characterController.Move(moveDirection * Time.deltaTime);
-
-        if(characterController.velocity.y!=0)
-        Debug.Log(characterController.velocity.y);
-
-        //if (characterVelocityMomentum.magnitude >= 0f)
-        //{
-        //    float momentumDrag = 10f;
-        //    characterVelocityMomentum -= characterVelocityMomentum * momentumDrag * Time.deltaTime;
-        //    if (characterVelocityMomentum.magnitude < .0f)
-        //    {
-        //        characterVelocityMomentum = Vector3.zero;
-        //    }
-        //}
     }
-
-    private void FixedUpdate()
-    {
-        //CharacterMove();
-    }
-
-    //private void CharacterMove()
-    //{
-    //    float moveSpeed = 0.5f;
-
-    //    // カメラの方向から、X-Z平面の単位ベクトルを取得
-    //    Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-
-    //    // 方向キーの入力値とカメラの向きから、移動方向を決定
-    //    Vector3 moveForward = cameraForward * _moveX + Camera.main.transform.right * _moveZ;
-
-    //    // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
-    //    //rb.velocity = moveForward * moveSpeed + new Vector3(0, rb.velocity.y, 0);
-    //    characterController.Move(moveForward * moveSpeed);
-
-    //    // キャラクターの向きを進行方向に
-    //    if (moveForward != Vector3.zero)
-    //    {
-    //        transform.rotation = Quaternion.LookRotation(moveForward);
-    //    }
-    //}
-
     private void ResetGravityEffect()
     {
         charcterVelocityY = 0f;
@@ -176,19 +134,11 @@ public class PlayerCharcterController : MonoBehaviour
 
         hookshotTransform.localScale = new Vector3(1, 1, hookshotSize);
 
-        Debug.Log("HookshotSize:" + hookshotSize);
-        Debug.Log("Distance:" + Vector3.Distance(transform.position, shotPoint));
-
         if (hookshotSize * wire.localScale.z > Vector3.Distance(transform.position, shotPoint))
         {
             hookshotSize = Vector3.Distance(transform.position, shotPoint) / wire.localScale.z;
             state = State.HookshotFlyingPlayer;
         }
-        //if (wireColisionScript.WireContact() == true)
-        //{
-        //    state = State.HookshotFlyingPlayer;
-        //}
-        //cameraFov.SetCameraFov(HOOKSHOT_FOV);
     }
     private void HookshotMovement()
     {
@@ -200,7 +150,7 @@ public class PlayerCharcterController : MonoBehaviour
         float hookshotSpeedMin = 10f;
         float hookshotSpeedMax = 40f;
 
-        //
+        //縮小スピード
         float hookshotSpeed = Mathf.Clamp(Vector3.Distance(transform.position, hookshotDirection), hookshotSpeedMin, hookshotSpeedMax);
 
         //加速
@@ -208,27 +158,26 @@ public class PlayerCharcterController : MonoBehaviour
 
         characterController.Move(hookshotDirection * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime);
 
-
         //ワイヤーの縮小
         hookshotSize -= (hookshotDirection * hookshotSpeed * hookshotSpeedMultiplier * Time.deltaTime).magnitude / wire.localScale.z;
         hookshotTransform.localScale = new Vector3(1, 1, hookshotSize);
 
         float reachedHookshotDistance = 1f;
+
         if (Vector3.Distance(transform.position, shotPoint) < reachedHookshotDistance)
         {
-            Debug.Log("実行");
             StopHookshot();
         }
 
-        if (TestInputDownHookshot())
+        if (Input.GetMouseButtonDown(1))
         {
             StopHookshot();
         }
 
-        if (TestInputJump())
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             float momentExtraSpeed = 7f;
-            float jumpSpeed = 40f;
+            float jumpSpeed = 10f;
             characterVelocityMomentum = hookshotDirection * hookshotSpeed * momentExtraSpeed;
             characterVelocityMomentum += Vector3.up * jumpSpeed;
             state = State.Normal;
@@ -240,14 +189,5 @@ public class PlayerCharcterController : MonoBehaviour
         state = State.Normal;
         ResetGravityEffect();
         hookshotTransform.gameObject.SetActive(false);
-        //cameraFov.SetCameraFov(NORMAL_FOV);
-    }
-    private bool TestInputDownHookshot()
-    {
-        return Input.GetMouseButtonDown(0);
-    }
-    private bool TestInputJump()
-    {
-        return Input.GetKeyDown(KeyCode.Space);
     }
 }
